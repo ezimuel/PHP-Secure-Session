3cf01b5be3643fe303a25a16e9ad26c73f4294e6
Jijo John <jijojohnx@outlook.com>
2015-07-12 13:02:05 +0530

    Fixed an issue
    There was an error locating the session directory. Fixed it with DIRECTORY_SEPARATOR predefined constant instead of using slash.

--- demo/demo.php
+++ demo/demo.php
@@ -13,15 +13,15 @@ require_once '../SecureSession.php';
 $sessionPath = sys_get_temp_dir();
 session_save_path($sessionPath);
 session_start();
+ if (empty($_SESSION['time'])) {
+     $_SESSION['time'] = time(); // set the time
+     $_SESSION['JIJO'] = 'HAI HOW ARE YOU';
 
-if (empty($_SESSION['time'])) {
-    $_SESSION['time'] = time();
-}    
+   }
+   $filename = $sessionPath . DIRECTORY_SEPARATOR . session_name() . '_' . session_id();
 
-$filename = $sessionPath . '/' . session_name() . '_' . session_id();
-
-echo "<h1>SecureSession Demo</h1>";
-echo "<br>Session created at <strong>" . date("G:i:s ", $_SESSION['time']) . "</strong>";
-echo "<br>Session file: <strong>" . $filename . "</strong>";
-echo "<br><br>Encrypted content:<br><pre>" . file_get_contents($filename). "</pre>";
-echo "<br><strong>Note:</strong> If you reload the page you see the encrypted data change each time";
+   echo "<h1>SecureSession Demo</h1>";
+   echo "<br>Session created at <strong>" . date("G:i:s ", $_SESSION['time']) . "</strong>";
+    echo "<br>Session file: <strong>" . $filename . "</strong>";
+    echo "<br><br>Encrypted content:<br><pre>" . file_get_contents($filename). "</pre>";
+    echo "<br><strong>Note:</strong> If you reload the page you see the encrypted data change each time";
